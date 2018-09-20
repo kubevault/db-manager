a1f81e9ac7c82e1406e0e0e25a9f5e6580f981e1
Tamal Saha <tamal@appscode.com>
2018-09-20 15:16:30 -0700

    Use --pull flag with docker build (#20)
    xref: https://github.com/appscodelabs/issues/issues/90
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/docker/make.sh
+++ hack/docker/make.sh
@@ -42,7 +42,7 @@ build_docker() {
   chmod 755 messenger
 
   cat >Dockerfile <<EOL
-FROM alpine
+FROM alpine:3.8
 
 RUN set -x \
   && apk add --update --no-cache ca-certificates
@@ -52,7 +52,7 @@ COPY messenger /usr/bin/messenger
 USER nobody:nobody
 ENTRYPOINT ["messenger"]
 EOL
-  local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
+  local cmd="docker build --pull -t $DOCKER_REGISTRY/$IMG:$TAG ."
   echo $cmd; $cmd
 
   rm messenger Dockerfile
